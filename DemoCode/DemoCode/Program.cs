a1013b82de9075f5b5702d08077b0d5015befd6b
Gytis <gyciauskas@hotmail.com>
2016-11-08 09:58:35 +0200

    Add a new project to repository.

---
+++ DemoCode/DemoCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change console foreground color to yellow
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("Hello Present Connection!");
+            Console.ReadLine();
+        }
+    }
+}
