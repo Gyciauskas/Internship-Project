b1e11e6a9868107045664ebcf2c0de11ae11c7df
Asta <astpetr@gmail.com>
2016-11-22 01:34:01 +0200

    testus pereina

---
+++ PresentConnection.Internship7.Iot.Domain/Lookup.cs
@@ -0,0 +1,20 @@
+﻿using CodeMash.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentConnection.Internship7.Iot.Domain
+{
+    [CollectionName("Lookups")]
+    public class Lookup : EntityBase
+    {
+        public Lookup() { }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public string Type { get; set; } // e.g. Widget, Category, Industry
+    }
+}
---
+++ PresentConnection.Internship7.Iot.Domain/Validators/LookupValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace PresentConnection.Internship7.Iot.Domain
+{
+    public class LookupValidator : AbstractValidator<Lookup>
+    {
+        public LookupValidator()
+        {
+            RuleFor(r => r.Name).NotEmpty();
+            RuleFor(r => r.Type).NotEmpty();
+        }
+    }
+}
