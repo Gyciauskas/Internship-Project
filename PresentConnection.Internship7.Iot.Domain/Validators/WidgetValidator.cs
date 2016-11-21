4f0ecc07d9236a73e0b4cc21b791ff578108930d
Delfx <motiejunas.lukas@gmail.com>
2016-11-22 00:25:55 +0200

    Created WidgetValidator

---
+++ PresentConnection.Internship7.Iot.Domain/Validators/WidgetValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentConnection.Internship7.Iot.Domain.Validators
+{
+    public class WidgetValidator : AbstractValidator<Widget>
+    {
+        public WidgetValidator()
+        {
+            RuleFor(r => r.Type).NotEmpty();
+            RuleFor(r => r.Query).NotEmpty();
+            RuleFor(r => r.Configuration).NotEmpty();
+
+        }
+    }
+}
