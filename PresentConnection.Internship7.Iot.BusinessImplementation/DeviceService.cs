f9de91e808e00bf960ef7758052301611081f66d
Asta <astpetr@gmail.com>
2016-11-24 15:42:16 +0200

    without PowerResourceType

---
+++ PresentConnection.Internship7.Iot.BusinessImplementation/DeviceService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeMash.Net;
+using FluentValidation.Results;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using PresentConnection.Internship7.Iot.BusinessContracts;
+using PresentConnection.Internship7.Iot.Domain;
+using PresentConnection.Internship7.Iot.Utils;
+
+namespace PresentConnection.Internship7.Iot.BusinessImplementation
+{
+    public class DeviceService : IDeviceService
+    {
+        public string CreateDevice(Device device)
+        {
+            DeviceValidator validator = new DeviceValidator();
+            ValidationResult results = validator.Validate(device);
+            bool validationSucceeded = results.IsValid;
+            if (validationSucceeded)
+            {
+                Db.InsertOne(device);
+                return device.Id.ToString();
+            }
+            else
+            {
+                throw new BusinessException("Cannot create device", results.Errors);
+            }
+        }
+
+        public void UpdateDevice(Device device)
+        {
+            DeviceValidator validator = new DeviceValidator();
+            ValidationResult results = validator.Validate(device);
+            bool validationSucceeded = results.IsValid;
+            if (validationSucceeded)
+            {
+                Db.FindOneAndReplace(x => x.Id == device.Id, device);
+            }
+            else
+            {
+                throw new BusinessException("Cannot update device", results.Errors);
+            }
+        }
+
+        public bool DeleteDevice(string id)
+        {
+            var deleteResult = Db.DeleteOne<Device>(x => x.Id == ObjectId.Parse(id));
+            return deleteResult.DeletedCount == 1;
+        }
+
+        public List<Device> GetAllDevices(string name = "")
+        {
+            var filterBuilder = Builders<Device>.Filter;
+            var filter = filterBuilder.Empty;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                var findByNameFilter = Builders<Device>.Filter.Eq(x => x.ModelName, name);
+                filter = filter & findByNameFilter;
+            }
+
+            var devices = Db.Find(filter);
+            return devices;
+        }
+
+        public Device GetDevice(string id)
+        {
+            return Db.FindOneById<Device>(id);
+        }
+    }
+}
---
+++ PresentConnection.Internship7.Iot.Domain/Validators/DeviceValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace PresentConnection.Internship7.Iot.Domain
+{
+    public class DeviceValidator : AbstractValidator<Device>
+    {
+        public DeviceValidator()
+        {
+            RuleFor(r => r.ModelName).NotEmpty();
+            RuleFor(r => r.UniqueName).NotEmpty();
+            RuleFor(r => r.Images.Count).GreaterThan(0);
+        }
+    }
+}
