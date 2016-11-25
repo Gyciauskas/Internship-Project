e0d1d3ec9a8a9adef200e2bf8a8c97208b0f94a2
Kestv <kestutis.ramulionis@gmail.com>
2016-11-25 17:11:15 +0200

    Fixed namespaces

---
+++ PresentConnection.Internship7.Iot.Domain/RecipeConnection.cs
@@ -0,0 +1,20 @@
+﻿using PresentConnection.Internship7.Iot.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentConnection.Internship7.Iot.Domain
+{
+    public class RecipeConnection
+    {
+        public string UniqueName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DisplayImage Image { get; set; }
+        public string ConnectionId { get; set; }
+        public string DeviceId { get; set; }
+        public string ComponentId { get; set; }
+    }
+}
