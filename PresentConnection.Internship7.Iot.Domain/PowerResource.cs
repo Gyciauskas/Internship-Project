fdbb446be862be6fd3f615dc1c520bf777228419
DESKTOP-QG6V83B\Domas <jonusas.dominykas@gmail.com>
2016-11-29 14:00:57 +0200

    no message

---
+++ PresentConnection.Internship7.Iot.Domain/PowerResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentConnection.Internship7.Iot.Domain
+{
+    public class PowerResource
+    {
+        public enum PowerResourceType { Battery, Voltage }
+        public int PercentageValue { get; set; }
+    }
+}
