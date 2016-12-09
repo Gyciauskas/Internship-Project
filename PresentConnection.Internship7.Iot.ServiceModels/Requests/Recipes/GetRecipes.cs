d12f0b188ea0bc5460ab20c929b061594636f8a2
Gytis <gyciauskas@hotmail.com>
2016-12-10 00:05:09 +0200

    commit some changes

--- PresentConnection.Internship7.Iot.ServiceModels/Requests/Recipes/GetRecipes.cs
+++ PresentConnection.Internship7.Iot.ServiceModels/Requests/Recipes/GetRecipes.cs
@@ -2,7 +2,7 @@
 
 namespace PresentConnection.Internship7.Iot.ServiceModels
 {
-    [Route("/recipes", "GET", Summary = "Gets all recipes or find all with same names!")]
+    [Route("/recipes", "GET", Summary = "Gets all recipes or with same name!")]
     public class GetRecipes : IReturn<GetRecipesResponse>
     {
         public string Name { get; set; }
