5ac2dcfab0306e6870c54be1978dfa223dc42ecb
DESKTOP-QG6V83B\Domas <jonusas.dominykas@gmail.com>
2016-12-21 22:38:06 +0200

    Fixed Client

---
+++ PresentConnection.Internship7.Iot.ServiceModels/Requests/Client/CreateClient.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+using PresentConnection.Internship7.Iot.Domain;
+using System.Collections.Generic;
+
+
+
+namespace PresentConnection.Internship7.Iot.ServiceModels
+{
+    [Route("/clients", "POST", Summary = "Create client")]
+    public class CreateClient
+    {
+        public string Name { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+    }
+}
